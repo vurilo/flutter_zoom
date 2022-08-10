d1b69294af3c5f33ed06a60f751aa46575b72c6a
phuyalrn22 <92909608+phuyalrn22@users.noreply.github.com>
2022-08-11 02:14:18 +0545

    zoom display Name

--- zoom/lib/zoom_view.dart
+++ zoom/lib/zoom_view.dart
@@ -38,6 +38,7 @@ class ZoomView extends ZoomPlatform {
   Future<bool> joinMeeting(ZoomMeetingOptions options) async {
     var optionMap = <String, String?>{};
     optionMap.putIfAbsent("userId", () => options.userId);
+    optionMap.putIfAbsent("displayName", () => options.displayName);
     optionMap.putIfAbsent("meetingId", () => options.meetingId);
     optionMap.putIfAbsent("meetingPassword", () => options.meetingPassword);
     optionMap.putIfAbsent("disableDialIn", () => options.disableDialIn);
